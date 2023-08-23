9c33ee03503c589ed69af22c27c0584c5b857746
Shubham Sharma <oc9lindvm@cossth.com>
2023-08-23 23:37:47 +0530

    Update Proxy Port

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,4 +1,10 @@
+import Axios from 'axios-observable';
+import { useEffect } from 'react';
+
 export function App() {
+  useEffect(() => {
+    Axios.get('/api').subscribe(console.log);
+  }, []);
   return (
     <div className="pagetitle">
       <h1>Hello World</h1>
