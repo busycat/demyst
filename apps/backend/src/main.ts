54baa2c9ca3463575cedc5295edf577502f9f786
Shubham Sharma <oc9lindvm@cossth.com>
2023-08-25 11:16:26 +0530

    Add dotenv , - Add Docker ENV Variables Support Later

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,7 @@
 import { BalanceSheets } from '@demyst/models';
 import express from 'express';
 import * as path from 'path';
+import { getBalanceSheet } from './controllers/balance-sheet';
 
 const app = express();
 
@@ -14,34 +15,7 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to backend!' });
 });
-app.get<unknown, BalanceSheets>('/api/balance-sheet', (req, res) => {
-  res.send([
-    {
-      year: 2020,
-      month: 12,
-      profitOrLoss: 250000,
-      assetsValue: 1234,
-    },
-    {
-      year: 2020,
-      month: 11,
-      profitOrLoss: 1150,
-      assetsValue: 5789,
-    },
-    {
-      year: 2020,
-      month: 10,
-      profitOrLoss: 2500,
-      assetsValue: 22345,
-    },
-    {
-      year: 2020,
-      month: 9,
-      profitOrLoss: -187000,
-      assetsValue: 223452,
-    },
-  ]);
-});
+app.get<unknown, BalanceSheets>('/api/balance-sheet', getBalanceSheet);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
