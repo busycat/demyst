ec52f3fd6dcad82a665c5f7ae9f68d02e3510738
Shubham Sharma <oc9lindvm@cossth.com>
2023-08-26 19:27:26 +0530

    Add Padding to Outlet

--- apps/frontend/src/app/app-chrome.tsx
+++ apps/frontend/src/app/app-chrome.tsx
@@ -1,25 +1,30 @@
-import { Toolbar, ToolbarButton } from '@fluentui/react-components';
+import { makeStyles, shorthands, tokens } from '@fluentui/react-components';
 import { FC, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import {
-  FontIncrease24Regular,
-  FontDecrease24Regular,
-  TextFont24Regular,
-} from '@fluentui/react-icons';
+import { Outlet } from 'react-router-dom';
 import { FluentProvider } from '@fluentui/react-components';
 import { getTheme } from './theme';
 import { Header } from './header';
 import { Footer } from './footer';
 
+const useStyles = makeStyles({
+  page: {
+    ...shorthands.margin(tokens.spacingVerticalL, tokens.spacingHorizontalL),
+  },
+});
+
 export const AppChrome: FC = () => {
+  const styles = useStyles();
   const [themeIsDark, setThemeIsDark] = useState(false);
+
   return (
     <FluentProvider
       theme={getTheme(themeIsDark)}
       style={{ minHeight: '100vh' }}
     >
       <Header onThemeChange={() => setThemeIsDark((p) => !p)} />
-      <Outlet />
+      <div className={styles.page}>
+        <Outlet />
+      </div>
       <Footer />
     </FluentProvider>
   );
--- apps/frontend/src/components/start-loan/start-loan.tsx
+++ apps/frontend/src/components/start-loan/start-loan.tsx
@@ -1,16 +1,34 @@
-import { Button, Title1 } from '@fluentui/react-components';
+import {
+  Button,
+  Spinner,
+  Title1,
+  makeStyles,
+  shorthands,
+  tokens,
+} from '@fluentui/react-components';
 import BusinessDetailsForm from '../business-details-form/business-details-form';
+import { useState } from 'react';
 
 /* eslint-disable-next-line */
 export interface StartLoanProps {}
 
+const useStyles = makeStyles({
+  page: {
+    ...shorthands.margin(tokens.spacingVerticalM, 'auto'),
+  },
+});
+
 export function StartLoan(props: StartLoanProps) {
-  return (
+  const [token, setToken] = useState<string>();
+
+  return token ? (
     <>
       <Title1>Start Loan</Title1>
       <BusinessDetailsForm />
       <Button>Submit</Button>
     </>
+  ) : (
+    <Spinner></Spinner>
   );
 }
 
